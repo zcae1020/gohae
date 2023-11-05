82062f515bbbe4eac8570742814a0936548a89cc
zaka <zcae1020@example.com>
2023-11-05 22:51:47 +0900

    feat(layout): nextjs layout 사용

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+export default function DashboardLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return <section>layout{children}</section>
+  }
---
+++ src/app/dashboard/not-found.tsx
@@ -0,0 +1,3 @@
+export default function DashboardNotFound() {
+    return <section>not-found</section>
+}

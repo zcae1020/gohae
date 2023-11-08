a802b12cbba62c8b623abda40598dfd5d594d077
zcae1020 <zcae1020@naver.com>
2023-11-08 22:31:03 +0900

    feat(menu): menu 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,19 @@ const Home = () => {
       </Card>
       <Card>
         <h1>Home</h1>
+        <Link href="/letterbox">편지함</Link>
+      </Card>
+      <Card>
+        <h1>Home</h1>
         <Link href="/write">편지쓰기</Link>
+      </Card>
+      <Card>
+        <h1>Home</h1>
+        <Link href="/letterbox">상점</Link>
+      </Card>
+      <Card>
+        <h1>Home</h1>
+        <Link href="/letterbox">더보기</Link>
       </Card>
     </>
   );

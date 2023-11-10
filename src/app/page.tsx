63f913fe99f458608934c318aba0a271bc068f16
zcae1020 <zcae1020@naver.com>
2023-11-10 21:58:07 +0900

    feat(nav): bottom nav 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,70 @@
-import { Card } from "@mui/material";
-import Link from "next/link";
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
+import { Restore } from "@mui/icons-material";
 
 import style from "./page.module.scss";
 
+const navigationData = [
+  {
+    label: "마이 페이지",
+    icon: <Restore />,
+    link: "/dashboard",
+  },
+  {
+    label: "편지함",
+    icon: <Restore />,
+    link: "/letterbox",
+  },
+  {
+    label: "편지쓰기",
+    icon: <Restore />,
+    link: "/write",
+  },
+  {
+    label: "상점",
+    icon: <Restore />,
+    link: "/store",
+  },
+  {
+    label: "더보기",
+    icon: <Restore />,
+    link: "/more",
+  },
+];
+
 const Home = () => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log("value", value);
+  }, [value]);
+
   return (
-    <div className={style.wrapper}>
-      <Card>
-        <Link href="/dashboard">마이 페이지</Link>
-      </Card>
-      <Card>
-        <Link href="/letterbox">편지함</Link>
-      </Card>
-      <Card>
-        <Link href="/write">편지쓰기</Link>
-      </Card>
-      <Card>
-        <Link href="/letterbox">상점</Link>
-      </Card>
-      <Card>
-        <Link href="/letterbox">더보기</Link>
-      </Card>
+    <div>
+      <Box className={style.wrapper} sx={{ width: 1000 }}>
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+            window.location.assign(navigationData[newValue].link);
+            // router.push(navigationData[newValue].link);
+          }}
+        >
+          {navigationData.map((item) => {
+            return (
+              <BottomNavigationAction
+                className={style.item}
+                key={item.label}
+                label={item.label}
+                icon={item.icon}
+              />
+            );
+          })}
+        </BottomNavigation>
+      </Box>
     </div>
   );
 };

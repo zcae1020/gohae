a127a6f27b68afb66dcc8e88456aae8fdedd6378
zcae1020 <zcae1020@naver.com>
2023-11-14 23:08:23 +0900

    feat(letterbox): 편지함 layout짜기

--- src/app/letterbox/page.tsx
+++ src/app/letterbox/page.tsx
@@ -1,24 +1,32 @@
-import { Card } from "@mui/material";
-import Link from "next/link";
+import { Button, CardContent } from "@mui/material";
+
+import style from "./page.module.scss";
 
 const Home = () => {
   return (
     <div>
-      <Card>
-        <Link href="/dashboard">마이 페이지</Link>
-      </Card>
-      <Card>
-        <Link href="/letterbox">편지함</Link>
-      </Card>
-      <Card>
-        <Link href="/write">편지쓰기</Link>
-      </Card>
-      <Card>
-        <Link href="/letterbox">상점</Link>
-      </Card>
-      <Card>
-        <Link href="/letterbox">더보기</Link>
-      </Card>
+      <div className={style.card}>
+        <CardContent>
+          <Button variant="contained" className={style.button}>
+            보낸 편지함
+          </Button>
+        </CardContent>
+        <CardContent>
+          <Button variant="contained" className={style.button}>
+            받은 편지함
+          </Button>
+        </CardContent>
+        <CardContent>
+          <Button variant="contained" className={style.button}>
+            편지 쓰기
+          </Button>
+        </CardContent>
+      </div>
+      {/* <List>
+        <li>편지 1</li>
+        <li>편지 2</li>
+        <li>편지 3</li>
+      </List> */}
     </div>
   );
 };
--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -2,27 +2,15 @@
 
 import React, { useState } from "react";
 
-import { Button, Card, CardContent, Input, List } from "@mui/material";
+import { Input, TextField } from "@mui/material";
 
 const Home = () => {
   const [value, setValue] = useState("");
 
   return (
     <div>
-      <Card>
-        <CardContent>
-          <Button>보낸 편지함</Button>
-        </CardContent>
-        <CardContent>
-          <Button>받은 편지함</Button>
-        </CardContent>
-      </Card>
-      <List>
-        <li>편지 1</li>
-        <li>편지 2</li>
-        <li>편지 3</li>
-      </List>
       <Input value={value} onChange={(e) => setValue(e.currentTarget.value)} />
+      <TextField label="Multiline" />
     </div>
   );
 };

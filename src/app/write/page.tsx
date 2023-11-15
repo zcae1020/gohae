963d0a60f1dd4a8af20c1647368f27086f205987
zcae1020 <zcae1020@naver.com>
2023-11-15 23:34:01 +0900

    feat(firebase): firebase 연결

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -2,15 +2,25 @@
 
 import React, { useState } from "react";
 
-import { Input, TextField } from "@mui/material";
+import { Button, Input, TextField } from "@mui/material";
+import { firestore } from "@/module/firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 const Home = () => {
   const [value, setValue] = useState("");
+
+  const onClick = () => {
+    addDoc(collection(firestore, "users"), {
+      name: value,
+    });
+    setValue("");
+  };
 
   return (
     <div>
       <Input value={value} onChange={(e) => setValue(e.currentTarget.value)} />
       <TextField label="Multiline" />
+      <Button onClick={onClick}>Button</Button>
     </div>
   );
 };

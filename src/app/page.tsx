d43d90e2a97880b084c32bb6ea288ae6737c3ad6
zcae1020 <zcae1020@naver.com>
2023-11-22 22:04:59 +0900

    feat(auth): google login 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@
 
 import React from "react";
 
+import { Button } from "@mui/material";
+import { signInGoogle } from "@/module/firebase";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Button onClick={signInGoogle}>로그인</Button>home
+    </div>
+  );
 };
 
 export default Home;
